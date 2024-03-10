3b9af26b24eef37f3b81638baece6fb7c400f5cd
Prathap Kumar <130935483+prathkr@users.noreply.github.com>
2024-03-10 23:27:46 +0530

    Create A. Vanya and Fence.cpp

---
+++ A. Vanya and Fence.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int n, h, p, wdth = 0;
+  cin >> n >> h;
+  for (int i = 0; i < n; i++) {
+    cin >> p;
+    if (p <= h) {
+      wdth += 1;
+    } else if (p > h) {
+      wdth += 2;
+    }
+  }
+  cout << wdth;
+}
